1562c3082f51bc09a927b133971b1a5bc78c9f3d
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2022-10-29 15:52:42 +0200

    Testing changes with CI/CD

--- bin/init.sh
+++ bin/init.sh
@@ -5,15 +5,15 @@
 Convenient script to ensure environment is ready to go
 
 ## Objectives
-- Ensure dependencies are installed
-- Auto configure for new + existing environments
-- Ensure it is executable without interactions (for CI/CD)
+  - Ensure dependencies are installed
+  - Auto configure for new + existing environments
+  - Ensure it is executable without interactions (for CI/CD)
 
 ## Wishlist
-- Identify and flag new dependency configs, maybe by comparing .env with .env.example
-- Optimise and introduce as a git hook
-- Auto spin up environment with sail
-- Ensure all dependencies are available, like `composer`, `php`, etc
+  - Identify and flag new dependency configs, maybe by comparing .env with .env.example
+  - Optimise and introduce as a git hook
+  - Auto spin up environment with sail
+  - Ensure all dependencies are available, like `composer`, `php`, etc
 '
 
 PATH_TO_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -25,21 +25,22 @@ echo "PATH_TO_REPO=$PATH_TO_REPO"
 if [ ! -f "$PATH_TO_REPO/.env" ]; then
   echo "Copied .env.example to .env"
   cp "$PATH_TO_REPO/.env.example" "$PATH_TO_REPO/.env"
-  php -r "file_exists('.env') || copy('.env.example', '.env');"
   php artisan key:generate
 fi
 
 source $PATH_TO_REPO/.env
 
-# Install composer dependencies
+echo "CLI_MODE: '${CI_MODE}'"
+
+if [ "${CI_MODE}" == "circleci" ]; then
+    composer install
+    php artisan migrate:refresh --seed --no-interaction --force
+    php artisan migrate:refresh --database=testing --seed --force --no-interaction
+    exit 0
+fi
+
 # TODO: Investigate why local is so much quicker, sees environmental
 # ./vendor/bin/sail composer install
 composer install
-
-# Migrate main database
-# ./vendor/bin/sail artisan migrate --seed --no-interaction --force
-php artisan migrate --seed --no-interaction --force
-
-# Migrate testing database
-# ./vendor/bin/sail artisan migrate --database=testing --seed --force --no-interaction
-php artisan migrate --database=testing --seed --force --no-interaction
+./vendor/bin/sail artisan migrate:refresh --seed --no-interaction --force
+./vendor/bin/sail artisan migrate:refresh --database=testing --seed --force --no-interaction
