c98f796d03b3dc544f2ee16b041f970d53cf9174
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2022-10-29 23:42:56 +0200

    Testing changes with CI/CD

--- bin/init.sh
+++ bin/init.sh
@@ -25,11 +25,12 @@ if [ ! -f "$PATH_TO_REPO/.env" ]; then
   php artisan key:generate
 fi
 
-source $PATH_TO_REPO/.env
-
 if [ "${CI_MODE}" == "circleci" ]; then
     echo "Running in CircliCI mode"
     echo "DB_HOST_OVERRIDE=127.0.0.1" >> "$PATH_TO_REPO/.env"
+
+    source $PATH_TO_REPO/.env
+
     composer install
     mysql -uroot -proot_password -e "DROP DATABASE IF EXISTS testing; CREATE DATABASE testing;"
 
@@ -41,7 +42,7 @@ if [ "${CI_MODE}" == "circleci" ]; then
 fi
 
 # TODO: Investigate why local is so much quicker, sees environmental
-# ./vendor/bin/sail composer install
+source $PATH_TO_REPO/.env
 composer install
 docker exec -it kata-mysql mysql -uroot -proot_password -e "DROP DATABASE IF EXISTS testing; CREATE DATABASE testing;"
 docker exec -it kata-mysql mysql -uroot -proot_password -e "GRANT ALL PRIVILEGES ON *.* TO 'sail'@'%'; FLUSH PRIVILEGES;"
