f6c7adb04862cca3fc1cceeaeb0e0843aa952780
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2022-11-19 06:23:07 +0100

    Fixed the user seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -3,23 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 
 class UsersSeeder extends BaseSeeder
 {
     public function seed(): void
     {
-        if (! is_null(User::first())) {
-            return;
-        }
-
-        // Ensure the id seed is reset
-        DB::table('users')->truncate();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
         $maxUsers = config('laravel-kata.dummy-data.max-users');
         if (User::count() >= $maxUsers) {
             return;
