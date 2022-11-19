772e608f7a115d3e839822715099283fb9c139a1
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2022-11-19 07:04:19 +0100

    Removed db migrate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        ParallelTesting::setUpProcess(function ($token) {
-            Artisan::call('migrate');
-        });
+        //
     }
 }
