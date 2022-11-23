2eae22b0e0b87a53208aa7975f4b02530012644c
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2022-11-23 09:27:18 +0100

    Debugging github-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    // site: 'https://hendrikprinsza.github.io',
+    base: '/laravel-kata',
     srcDir: './client/src',
     publicDir: './client/public',
     outDir: './client/dist'
