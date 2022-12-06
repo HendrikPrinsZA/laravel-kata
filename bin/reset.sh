b110fd4182011b48fd1d2a581a14a2e7d41d22a4
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2022-12-06 21:02:31 +0100

    Fixing CI/CD issues

--- bin/reset.sh
+++ bin/reset.sh
@@ -20,27 +20,26 @@ Convenient script to ensure environment is ready to go
 PATH_TO_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PATH_TO_REPO="$PATH_TO_SCRIPT_DIR/../"
 
+echo "PATH_TO_REPO is '${PATH_TO_REPO}'"
+echo "Listing files..."
+ls -la $PATH_TO_REPO
+
+# Always load the example env
 if [ ! -f "$PATH_TO_REPO/.env" ]; then
-    if [ "${CI_MODE}" == "railway" ]; then
-        echo "Copied .env.example to .env"
-        cp "$PATH_TO_REPO/.env.railway" "$PATH_TO_REPO/.env"
-    else
-        echo "Copied .env.example to .env"
-        cp "$PATH_TO_REPO/.env.example" "$PATH_TO_REPO/.env"
-        php artisan key:generate
-    fi
+    cp "$PATH_TO_REPO/.env.example" "$PATH_TO_REPO/.env"
 fi
 
-# TODO: Figure out how to best link storage os agnostic
-# Generic commands
-# php artisan storage:link
-
+# Append the railway env
 if [ "${CI_MODE}" == "railway" ]; then
     echo "Running in Railway"
-
+    echo "\n# Railway\n" >> $PATH_TO_REPO/.env
+    cat $PATH_TO_REPO/.env.railway >> $PATH_TO_REPO/.env
     source $PATH_TO_REPO/.env
 
     php artisan migrate --seed --no-interaction --force
+
+    # TODO: Sync main production database to start with some base data
+    # - Performance, test with some load (exchange rates 20 years back)
     exit 0
 fi
 
@@ -60,6 +59,14 @@ if [ "${CI_MODE}" == "circleci" ]; then
     php artisan migrate:refresh --database=testing --seed --force --no-interaction
     exit 0
 fi
+
+
+# Load base variables
+source $PATH_TO_REPO/.env
+
+# TODO: Figure out how to best link storage os agnostic
+# Generic commands
+# php artisan storage:link
 
 # TODO: Investigate why local is so much quicker, sees environmental
 source $PATH_TO_REPO/.env
