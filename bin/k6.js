9423684279b5604e54acf0d1b2a070d068380e34
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2022-12-17 21:00:51 +0100

    WIP: Grafana + InfluxDB + k6

--- bin/k6.js
+++ bin/k6.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import { sleep } from 'k6';
+import { sleep } from "k6";
 
 /**
  * Integrate load tests with CI/CD
@@ -7,32 +7,79 @@ import { sleep } from 'k6';
  * Refs
  * - https://k6.io/blog/integrating-load-testing-with-circleci/
  * - https://circleci.com/developer/orbs/orb/k6io/test
+ * - https://k6.io/docs/examples/advanced-api-flow/
  */
+const CLASS = 'KataChallengeEloquent';
+const METHOD = 'getCollectionCount';
+const MODE = 'Before';
+// const MODE = 'Record';
 
-const URL = 'http://localhost/api/kata/KataChallengeMySQL/orVersusIn';
-// const URL = 'http://localhost/api/kata/KataChallengeMySQLRecord/getCollectionUnique';
-
-export let options = {
+export const options = {
     vus: 5,
     stages: [
         { duration: "10s", target: 10 },
-        { duration: "30s", target: 20 },
-        { duration: "1m", target: 100 },
+        { duration: "1m", target: 50 },
         { duration: "30s", target: 0 },
-    ]
+    ],
+    // thresholds: {
+    //   'http_req_duration': ['p(95)<20000', 'p(99)<30000'],
+    // },
+    ext: {
+        loadimpact: {
+            projectID: 3620115,
+            name: CLASS,
+        },
+    },
 };
 
+// Placeholder for authentication
 export function setup() {
-  return {
-    'token': '123456'
-  }
+    return {
+        token: "123456",
+    };
 }
 
-export default function (data) {
-  http.get(URL);
-  sleep(1);
-}
+let iterations = 1;
 
-export function teardown(data) {
-//   console.log('teardown()');
-}
+export default (authToken) => {
+    const requestConfigWithTag = (tag) => ({
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        },
+        tags: Object.assign(
+            {},
+            {
+                name: 'DefaultTag',
+            },
+            tag
+        ),
+        tag
+    });
+
+    const payload = {};
+
+    iterations++;
+
+    if (MODE === 'Before') {
+        const url = `http://localhost/api/kata/${CLASS}/${METHOD}`;
+        const params = {
+            iterations
+        }
+        const res = http.request('GET', url, params, requestConfigWithTag({
+            name: 'Before'
+        }));
+    }
+
+    if (MODE === 'Record') {
+        const classRecord = `${CLASS}Record`;
+        const url = `http://localhost/api/kata/${classRecord}/${METHOD}`;
+        const params = {
+            iterations
+        }
+        const res = http.request('GET', url, params, requestConfigWithTag({
+            name: 'Record'
+        }));
+    }
+
+    sleep(1);
+};
