d88189ccf8489235188c7fb330569357f08769b3
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2023-05-11 22:57:26 +0200

    Fixing tests (6)

--- bin/reset.sh
+++ bin/reset.sh
@@ -41,7 +41,6 @@ fi
 
 if [ "${CI_MODE}" == "circleci" ]; then
     echo "Running in CircliCI"
-    echo $'\n# CircliCI\n' >> $PATH_TO_REPO/.env
     echo "DB_HOST_OVERRIDE=127.0.0.1" >> "$PATH_TO_REPO/.env"
     source $PATH_TO_REPO/.env
 
@@ -50,17 +49,16 @@ if [ "${CI_MODE}" == "circleci" ]; then
     docker exec -it kata-mysql mysql -uroot -p$DB_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS $DB_TEST_DATABASE; CREATE DATABASE $DB_TEST_DATABASE;"
     docker exec -it kata-mysql mysql -uroot -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USERNAME'@'%'; FLUSH PRIVILEGES;"
 
-    ./vendor/bin/sail artisan migrate:fresh --database=$DB_DATABASE --env=$APP_ENV --seed --force --no-interaction
+    ./vendor/bin/sail artisan key:generate --no-interaction
+    ./vendor/bin/sail artisan migrate:fresh --database=$DB_TEST_DATABASE --seed --force --no-interaction
+    ./vendor/bin/sail artisan migrate:fresh --database=$DB_DATABASE --env=testing --seed --force --no-interaction
 
-    export DB_TEST_DATABASE=testing
-    export DB_DATABASE=$DB_TEST_DATABASE
-    ./vendor/bin/sail artisan migrate:fresh --database=$DB_TEST_DATABASE --env=testing --seed --force --no-interaction
+    # export DB_TEST_DATABASE=testing
+    # export DB_DATABASE=$DB_TEST_DATABASE
+    # ./vendor/bin/sail artisan migrate:fresh --database=$DB_TEST_DATABASE --env=testing --seed --force --no-interaction
 fi
 
 if [ "${CI_MODE}" == "local" ]; then
-    # echo "in here"
-    # exit 0;
-
     echo "Running in local"
     source $PATH_TO_REPO/.env
 
@@ -69,9 +67,6 @@ if [ "${CI_MODE}" == "local" ]; then
 
     # Install dependencies
     ./vendor/bin/sail composer install
-
-    # Generate app key
-    # ./vendor/bin/sail artisan key:generate --no-interaction
 
     docker exec -it kata-mysql mysql -uroot -p$DB_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS $DB_DATABASE; CREATE DATABASE $DB_DATABASE;"
     docker exec -it kata-mysql mysql -uroot -p$DB_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS $DB_TEST_DATABASE; CREATE DATABASE $DB_TEST_DATABASE;"
