e2dea2b3e3c078280b082fd6ba3a558ee608adf6
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2023-05-12 12:27:45 +0200

    Fixed astro issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,21 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
-
-import * as dotenv from 'dotenv'
-dotenv.config()
-
+import * as dotenv from 'dotenv';
+import tailwind from "@astrojs/tailwind";
+dotenv.config();
 let site = null;
 
-// https://astro.build/config
 let config = {
-    base: '/laravel-kata',
-    srcDir: './client/src',
-    publicDir: './client/public',
-    outDir: './client/dist',
-    integrations: [
-      vue(),
-      svelte()
-    ]
+  base: '/laravel-kata',
+  srcDir: './client/src',
+  publicDir: './client/public',
+  outDir: './client/dist',
+  integrations: [vue(), svelte()]
 };
-
 if (process.env.CI_MODE === 'local') {
   config.site = 'http://localhost/laravel-kata';
 }
 
+// https://astro.build/config
 export default defineConfig(config);
