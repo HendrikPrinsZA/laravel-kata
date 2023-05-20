c729c0cb6d544c4278954586a0c7f2288bd47fa5
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2023-05-20 16:54:07 +0200

    Simplified benchmark script

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -2,23 +2,14 @@
 
 PATH_TO_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PATH_TO_REPO="$PATH_TO_SCRIPT_DIR/../"
-
 source $PATH_TO_REPO/.env
 
 if [ "${CI_MODE}" == "circleci" ]; then
     echo "LK_RUN_MODE=benchmark" >> "$PATH_TO_REPO/.env"
     php artisan kata:run --all
-
-    exitCode=$?
-    if [ $exitCode -ne 0 ]; then
-        echo "Error: Kata run failed"
-        exit $exitCode
-    fi
-
-    exit 0
+else
+    ./vendor/bin/sail artisan kata:run --all
 fi
-
-./vendor/bin/sail artisan kata:run --all
 
 exitCode=$?
 if [ $exitCode -ne 0 ]; then
