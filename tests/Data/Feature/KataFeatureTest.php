4cb6ef1a3f0b160c508f34758dcf8774b8a69e2a
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2023-05-29 20:31:30 +0200

    Simplified feature test to only run Sample challenge

--- tests/Data/Feature/KataFeatureTest.php
+++ tests/Data/Feature/KataFeatureTest.php
@@ -2,12 +2,6 @@
 
 namespace Tests\Data\Feature;
 
-use Illuminate\Support\Facades\Config;
-
-beforeEach(function () {
-    Config::set('laravel-kata.gains-perc-minimum', -1);
-});
-
 it('can connect', function () {
     $this->get('/')->assertStatus(200);
     $this->assertTrue(true);
@@ -45,16 +39,14 @@ test('get challenge methods', function (array $challenges) {
     return $challengeMethods;
 })->depends('get challenges');
 
-test('run challenge', function ($challenges) {
-    foreach ($challenges as $challenge) {
-        $response = $this->get(sprintf('/api/kata/%s/run', $challenge), [
-            'iterations' => 1,
-        ])->assertStatus(200);
+test('run challenge', function () {
+    $response = $this->get('/api/kata/Sample/run', [
+        'iterations' => 1,
+    ])->assertStatus(200);
 
-        $this->assertJsonResponseFormat($response, [
-            'success' => 'boolean',
-            'data' => 'array',
-            'data.report' => 'array',
-        ]);
-    }
-})->depends('get challenges');
+    $this->assertJsonResponseFormat($response, [
+        'success' => 'boolean',
+        'data' => 'array',
+        'data.report' => 'array',
+    ]);
+});
