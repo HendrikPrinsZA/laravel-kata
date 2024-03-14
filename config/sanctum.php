6c180b72f961b507c16b6e0080023ed7d9cb2710
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2024-03-14 22:32:06 +0200

    Upgraded to Laravel v11

--- config/sanctum.php
+++ config/sanctum.php
@@ -58,10 +58,10 @@ return [
     | request. You may change the middleware listed below as required.
     |
     */
-
     'middleware' => [
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
-        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
+        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
+        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
+        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
 ];
